a0349bffa504d12009e01e169bba635f0d50c69c
Avaneesa Basappa <avaneesab5@gmail.com>
2016-11-21 14:27:14 -0500

    Changed a function name

---
+++ genEncodedOutput.m
@@ -0,0 +1,16 @@
+function [sigLength,OP] = genEncodedOutput(F1,F2,F3)
+    % first digit
+    sigLength = 1:1028;
+    amp = 10; % amplitude
+    SigOne = [F1,F2,F3]; % Hz
+    fs = 4096; % Hz
+    Ts = 1/fs;
+    OutPut = zeros(1,1028);
+    %n = 1:signal_length;
+    for index=1:length(SigOne)
+    OutPut = OutPut + (amp * cos(2 * pi * SigOne(index) * sigLength * Ts));   
+    end
+    %OP = {sigLength,OutPut};% cell array
+    sigLength = sigLength;
+    OP = OutPut;% cell array
+end

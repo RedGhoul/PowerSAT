0c69411311922b5b975f354d24f693b3b7f48a1b
Avaneesa Basappa <avaneesab5@gmail.com>
2016-11-20 22:49:39 -0500

    implemented the noise function, had a few questions

--- startNoise.m
+++ startNoise.m
@@ -1,7 +1,12 @@
-function [ output_args ] = Noise( input_args )
+function [sigArrayOut] = Noise(sigArray,NoiseSquare)
 %NOISE Summary of this function goes here
 %   Detailed explanation goes here
-
-
+    signalSize = length(sigArray);
+    noise = randn(1,signalSize) .* -1; % should this be postive noise ?
+    % since when you sqaure it will be postive anyways ?
+    for index=1:NoiseSquare
+        noise = noise.^2;
+    end
+    sigArrayOut = sigArray + noise;
 end
 

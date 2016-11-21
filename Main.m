03ce3b2a59b96e864a78e0f9cb7c43d112e33e6e
Avaneesa Basappa <avaneesab5@gmail.com>
2016-11-20 22:53:12 -0500

    the more I do the encoder the more question I have lol

--- Main.m
+++ Main.m
@@ -1,5 +1,7 @@
 disp('meh 1')
-
-[len,output] = getencodedOutput(150,200,300);
+% if feel like I can finally use GUIDE over here yay
+% how do you want use to the MATLAB part ? A train of zeros and 
+% and some signals ? Or everything in time ?
+[len,output] = getencodedOutput(150,200,300); % for the digit 1
 
 plot(len,output)

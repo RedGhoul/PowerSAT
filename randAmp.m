0233e9273a40d29b851dfe952407d31981d28fd7
Avaneesa Basappa <avaneesab5@gmail.com>
2016-11-20 21:38:42 -0500

    added some more functions

---
+++ randAmp.m
@@ -0,0 +1,7 @@
+function [ output_args ] = RandAmp( input_args )
+%RANDAMP Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
---
+++ startNoise.m
@@ -0,0 +1,7 @@
+function [ output_args ] = Noise( input_args )
+%NOISE Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+

bd9fefb364a62dd7af0478b66d08853759aeb9ad
Avaneesa Basappa <avaneesab5@gmail.com>
2016-11-22 16:12:25 -0500

    Added a TODO file, and removed comments

---
+++ TODO.m
@@ -0,0 +1,16 @@
+    % one N = 1 millisec ?
+    
+    %Does this part need to be random or can it be exact ?
+    %The duration of each digit is 50 milliseconds, 
+    % with 50 milliseconds silence between subsequent
+    % digits.
+    
+    % when you say that the amp has to be between 1 and 2, 
+    % do you mean to say that each and every point has to 
+    % hold to that rule, before noise?
+    
+    %Fs and Ts ? - RUA
+    
+    %
+    
+    
--- genEncodedOutput.m
+++ genEncodedOutput.m
@@ -1,15 +1,12 @@
 function [sigLength,OP] = genEncodedOutput(F1,F2,F3)
-    % one N = 1 millisec ?
-    %The duration of each digit is 50 milliseconds, 
-    %with 50 milliseconds silence between subsequent
-    %digits.
-    sigLength = 1:50; % ???? WTF
+
+    sigLength = 1:50; 
     amp = rand() + 1; % amplitude For each tone, the amplitude is
              % different.
     SigOne = [F1,F2,F3]; % Hz
     fs = 4096; % Hz
     Ts = 1/fs;
-    OutPut = zeros(1,50); % ???? WTF
+    OutPut = zeros(1,50);
     %n = 1:signal_length;
     for index=1:length(SigOne)
     OutPut = OutPut + (amp * cos(2 * pi * SigOne(index) * sigLength * Ts));   

886e37531e87e46b60a3b19253cf06ce7505630a
Avaneesa Basappa <avaneesab5@gmail.com>
2016-11-22 16:21:02 -0500

    made my questions

--- TODO.m
+++ TODO.m
@@ -1,16 +1,32 @@
-    % one N = 1 millisec ?
     
-    %Does this part need to be random or can it be exact ?
-    %The duration of each digit is 50 milliseconds, 
+    %Sig generation
+    
+    % can we view this as one massive array, and everything is a train ?
+    % one N = 1 millisec ?
+    % what exactly does the index mean ? or supposed to mean ?
+    % is each index in freq refer to a freq ?
+    
+    % Does this part need to be random or can it be exact ?
+    % The duration of each digit is 50 milliseconds, 
     % with 50 milliseconds silence between subsequent
     % digits.
     
     % when you say that the amp has to be between 1 and 2, 
     % do you mean to say that each and every point has to 
     % hold to that rule, before noise?
+    % do we have to go through each and every point ?
     
     %Fs and Ts ? - RUA
     
-    %
+    % Delays
+    
+    % Do we need any varition in the delays ?
+    % can the delay just be concatnating some zeros on both sides ?
+    
+    % Noise
+    % noise = randn(1,signalSize) .* -1; % should this be postive noise ?
+    % since when you sqaure it will be postive anyways ? Since your just
+    % sqauring stuff
+    % when you add noise it's amp does not have to be between 1 and 2 ?
     
     

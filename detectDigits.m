9796dd72e8f8ee69f4dd007231882cb39d79a562
Avaneesa Basappa <avaneesab5@gmail.com>
2016-11-30 02:04:38 -0500

    added the sudo code to detectdigits

--- detectDigits.m
+++ detectDigits.m
@@ -2,6 +2,12 @@ function [digitsDetected,errorPercentage] = detectDigits(energySignal)
 %DETECTDIGITS Summary of this function goes here
 %   Detailed explanation goes here
 % do some magic
+% use the hilbert function to make a nice clean envolope
+% graph a couple of lines for the threshold values
+% then find the intersections of between the lines and envolope
+% then find the distances between the points
+% to see if they are right
+% based on the difference, classify it into a digit
         errorPercentage = 0;
         digitsDetected = 0;
 end

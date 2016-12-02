6a59a431e80c4db25fa85572e7c04e243a387b2a
Avaneesa Basappa <avaneesab5@gmail.com>
2016-12-02 03:02:09 -0500

    made the energy function that computes all the energys over the different frequencies

--- computeEng.m
+++ computeEng.m
@@ -1,8 +1,8 @@
-function [energys] = computeEnergySig(incomingTimeSignal)
+function [energys] = computeEng(incomingTimeSignal)
 %COMPUTEENERGYSIG Summary of this function goes here
 %   Detailed explanation goes here
     freqs = [100,150,200,250,300,350,400];
-    Energys = [];
+    
     samplingFreq = 2048;
     sigLength = length(incomingTimeSignal);
     windowSize = round(sigLength/6);
@@ -16,11 +16,15 @@ function [energys] = computeEnergySig(incomingTimeSignal)
             fftWindow = fft(Window(:, shift+1).*hamWindow);
             outPutEnergySig(shift+1) = abs(fftWindow(binNumber))^2; % we are trying to get the energy each time, we do the DFT shift window
         end
-        Energys(freqNumber) = outPutEnergySig;
+        Energys(freqNumber,:) = outPutEnergySig;
         outPutEnergySig = 0;
-        Window = 0;
+        
     end
-    
+    figure
+    for cows=1:length(freqs)
+        plot(Energys(cows,:))
+        hold on
+    end
     energys = Energys;
    
 end
